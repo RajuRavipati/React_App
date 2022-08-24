8fd8b68174eb6c296968de875151290b7d876cf4
RajuRavipati <gouthamravipati@gmail.com>
2022-08-24 18:00:47 +0530

    React-App

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         REACT JS 
+        </p>
+        <a
+          className="App-link"
+          href="https://fb.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          MENU
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
